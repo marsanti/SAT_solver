857e2ce76098094b9dbdbde0e84befec5f4bb97b
Marco Santi <marcosanti01@gmail.com>
2023-12-27 09:10:53 +0100

    Added test3

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
 //        formula.addClause(clause2);
 //
 //        System.out.println("F = " + formula);
-        Formula f = Utils.readFormulaFromFile("tests/test2.cnf");
+        Formula f = Utils.readFormulaFromFile("tests/test3.cnf");
         CDCL solver = new CDCL(f);
         solver.findModel();
     }
