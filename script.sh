60f084295c5ccb59f6e35bb2794cf00d91f1610d
Marco Santi <marcosanti01@gmail.com>
2024-01-31 12:29:27 +0100

    script

--- script.sh
+++ script.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-if [[ -z $1 ]];
+if [[ -z $1 ]] || [[ ! -z $5 ]];
 then
     echo "Wrong usage: ./script.sh -help"
 else
-    java -jar SAT_solver.jar "$1"
+    if [[ "$1" == *"-help"* ]];
+    then
+        echo "Using script you need to substitute SAT_solver.jar with ./script.sh in order to use the script."
+        echo
+        java -jar SAT_solver.jar "$1"
+    else
+        if [[ -z $2 ]];
+        then 
+            if [[ -z $3 ]];
+            then 
+                if [[ -z $4 ]];
+                then 
+                    java -jar SAT_solver.jar "$1" "$2" "$3" "$4"
+                else
+                    java -jar SAT_solver.jar "$1" "$2" "$3"
+                fi
+            else
+                java -jar SAT_solver.jar "$1" "$2"
+            fi
+        else
+            echo "Wrong usage: ./script.sh -help"
+        fi
+    fi
 fi
